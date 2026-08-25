9b8caa82606fc5d3e9e4ff81541781e98dfa1481
Ali Kurnaz <alikurnaz_26@windowslive.com>
2026-08-25 18:17:16 +0300

    feat: implement PageRequest class with pagination logic and validation

---
+++ lib/src/page_request.dart
@@ -0,0 +1,34 @@
+import '../very_commons.dart';
+
+class PageRequest implements Pageable {
+  @override
+  final int page;
+
+  @override
+  final int size;
+
+  @override
+  final Sort sort;
+
+  const PageRequest({required this.page, required this.size, required this.sort})
+    : assert(page >= 0, "Page index must not be less than zero!"),
+      assert(size >= 1, "Page size must not be less than one!");
+
+  @override
+  int get offset => page * size;
+
+  @override
+  PageRequest next() => PageRequest(page: page + 1, size: size, sort: sort);
+
+  @override
+  PageRequest previous() => page == 0 ? this : PageRequest(page: page - 1, size: size, sort: sort);
+
+  @override
+  PageRequest first() => PageRequest(page: 0, size: size, sort: sort);
+
+  @override
+  Pageable withPage(int page) => PageRequest(page: page, size: size, sort: sort);
+
+  @override
+  bool hasPrevious() => page > 0;
+}
--- lib/very_commons.dart
+++ lib/very_commons.dart
@@ -1,5 +1,6 @@
 library;
 
+export 'src/page_request.dart';
 export 'src/pageable.dart';
 export 'src/slice.dart';
 export 'src/sort.dart';
